053594cbdbfaacca21b1bd02b48331a350c61d16
David Ramirez <david.9706@hotmail.com>
2021-10-03 20:54:47 -0700

    Sign Up Page
    Routing works, however, the page needs to take the new component.css so the sign up page can be displayed. signup.component.css and html contain the same as the creatingaccount.html. Not sure about errors with implementing bootstrap. it needs further implementations.

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
