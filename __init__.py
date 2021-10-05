55562c7e44c957f354599ac662594081b4605b28
chrisTobar1999 <72772248+chrisTobar1999@users.noreply.github.com>
2021-10-05 11:54:56 -0700

    Created basic API
    its cool

--- __init__.py
+++ __init__.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
+import databases
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/api/showTables')
+def test():
+    return databases.fetchTables()
 
 app.run()
