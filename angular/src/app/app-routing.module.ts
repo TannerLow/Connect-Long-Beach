67bf6ca7c259dd274dd5c732f35b77791c3221ce
David Ramirez <david.9706@hotmail.com>
2021-10-14 00:05:26 -0700

    Default page bug fixed
    The app will open on the log in page. Previous bug was fixed.

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import {LogInComponent} from "./log-in/log-in.component";
 
 const routes: Routes = [
+  {path: '', redirectTo:'/log-in', pathMatch:"full"},
   {path: 'sign-up', component: SignUpComponent},
-  {path: 'log-in', component: LogInComponent}
+  {path: 'log-in', component: LogInComponent},
 ];
 
 
