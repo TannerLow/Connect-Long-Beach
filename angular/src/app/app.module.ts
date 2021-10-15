b619e074f1dfa1919c5d87e6aaf46e1f1400faf6
David Ramirez <david.9706@hotmail.com>
2021-10-14 22:57:30 -0700

    Profile Design
    Angular material has been implemented to provide an easy way to design UI.

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
 import { LogInComponent } from './log-in/log-in.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatDividerModule} from "@angular/material/divider";
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    LogInComponent
+    LogInComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
