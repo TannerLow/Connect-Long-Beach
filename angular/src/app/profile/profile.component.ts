253593ef38f70c786272d464b622d7fce8410570
David Ramirez <david.9706@hotmail.com>
2021-11-06 15:47:56 -0700

    Picture Dialog Working

--- angular/src/app/profile/profile.component.ts
+++ angular/src/app/profile/profile.component.ts
@@ -2,12 +2,15 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 //Data used for the Dialog data.. might want to export this interface to save space
 export interface DialogData {
+  firstName: string;
+  lastName: string;
   major: string;
   year: string;
-  gender: string;
+  selectedGender: string [];
   interest: string;
-  courses: string;
-  pa: string;
+  selectedCourses: string [];
+  pa: any;
+  url: any;
 }
 
 
@@ -17,12 +20,13 @@ export interface DialogData {
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements  OnInit{
-
+  firstName = ""
+  lastName = ""
   major = ""
   year = ""
-  gender = ""
+  selectedGender = [];
   interest = ""
-  courses = ""
+  selectedCourses = [];
   pa = "/static/assets/anonymous.png"
   constructor(public dialog: MatDialog) {}
   ngOnInit(): void {
@@ -31,21 +35,27 @@ export class ProfileComponent implements  OnInit{
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
       width: '30%',
-      data: {major: this.major, year: this.year, gender: this.gender, interest: this.interest, courses: this.courses, pa: this.pa}
+      data: {firstName: this.firstName, lastName: this.lastName,major: this.major, year: this.year, selectedGender: this.selectedGender, interest: this.interest, selectedCourses: this.selectedCourses, pa: this.pa}
     });
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
-      this.major = result[0];
-      this.year = result[1];
-      this.gender = result[2];
-      this.interest = result[3];
-      this.courses = result[4];
+      console.log(result)
+      this.firstName = result[0];
+      this.lastName = result[1];
+      this.major = result[2];
+      this.year = result[3];
+      this.selectedGender = result[4];
+      this.interest = result[5];
+      this.selectedCourses = result[6];
+      this.pa = result[7];
+      console.log(this.firstName);
+      console.log(this.lastName);
       console.log(this.major);
       console.log(this.year);
-      console.log(this.gender);
+      console.log(this.selectedGender);
       console.log(this.interest);
-      console.log(this.courses);
+      console.log(this.selectedCourses);
 
     });
   }
@@ -60,6 +70,8 @@ export class ProfileComponent implements  OnInit{
 export class DialogOverviewExampleDialog implements OnInit{
   classCourses : string[] = ['CECS 324', 'CECS 225', 'CECS 328', 'CECS 343', 'CECS 451', 'CECS 453'];
   genderNames : string[] = ['MALE', 'FEMALE', 'LESBIAN', 'GAY', 'HETEROSEXUAL', 'BISEXUAL'];
+
+
   constructor(
     public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
@@ -68,9 +80,13 @@ export class DialogOverviewExampleDialog implements OnInit{
   }
 
   onFileSelected(event){
-    console.log(event);
+    const files = event.target.files;
+    const reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.data.pa = reader.result;
+    }
   }
-  getGender(){
-  }
+
 
 }
