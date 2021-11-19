457ef84476e2c607a1043ae92c81bb89bc150c1a
chrisTobar1999 <72772248+chrisTobar1999@users.noreply.github.com>
2021-11-19 11:34:52 -0800

    Slight tweak to database call

--- databases.py
+++ databases.py
@@ -232,17 +232,20 @@ def like_unlike_post(mysql, user_id, post_id):
     cur = mysql.connection.cursor()
     
     cur.execute(f"SELECT * FROM posts WHERE post_id={post_id};")
-
+    data = cur.fetchone()
+    
     #Checks to see if it exists or not within the database
-    if cur.fetchone():
+    if data:
         #Since it exists, we are going to "Unlike" the post
         cur.execute(f"DELETE FROM userLikes WHERE post_id = {post_id} AND user_id = {user_id};")
+        response["response"] = True
     else:
         #Post hasn't been liked by user, so we create an entry into the database for it
         cur.execute(f"INSERT INTO userLikes(user_id,post_id) VALUES ({user_id},{post_id});")
+        response["response"] = True
 
     mysql.connection.commit()
-    response["response"] = True
+    
     cur.close()
     return response
 
