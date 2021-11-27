48924ea3f14308383ee5759b015f79be99cb52f5
Lesly Silva <silva.lesly2119@gmail.com>
2021-11-26 16:04:47 -0800

    uploading images

--- angular/src/app/home-page/home-page.component.ts
+++ angular/src/app/home-page/home-page.component.ts
@@ -6,10 +6,25 @@ import { Component, NgModule, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  firstName = "CONNECT LONG"
+  lastName = "BEACH"
+  postText = "Here is a picture of the Walter Pyramid at the University of Long Beach."
+  privacy = "Public"
+  currentDate = "Nov 10 2021, 2:00pm" 
+  likes = "12"
+
+  uploadedFile = null;
 
   constructor() { }
-
+  
   ngOnInit(): void {
+  }
+
+  onFileSelected(event){ //takes element(file) and 
+    this.uploadedFile = event.target.files[0];
+  }
+  onUpload()//function will upload file onto database when post is clicked 
+  {
   }
 
 }
